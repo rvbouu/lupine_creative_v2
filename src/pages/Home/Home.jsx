01a8de5767c66f61429dbc7896a643fef630db27
Vanessa Bou <rvbouu@gmail.com>
2024-06-26 01:48:02 -0500

    finished styling home skeleton

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,23 @@
+import './Home.css'
 
-export default function Home(){
+export default function Home() {
 
-  return(
+  return (
     <section className="viewport">
-    <h1>This is the Home Page</h1>
+      <section className="main-product">
+        <img src="/placeholder.jpg" alt="placeholder.jpg" className='main-image'/>
+        <h1 id='main-title'>Explore Our Artisan Handmade Crafts!</h1>
+
+      </section>
+      <section className="carousel">
+        <img className='placeholder' src="/placeholder.jpg" alt="placeholder.jpg" />
+        <img className='placeholder' src="/placeholder.jpg" alt="placeholder.jpg" />
+        <img className='placeholder' src="/placeholder.jpg" alt="placeholder.jpg" />
+        <img className='placeholder' src="/placeholder.jpg" alt="placeholder.jpg" />
+        <img className='placeholder' src="/placeholder.jpg" alt="placeholder.jpg" />
+        <img className='placeholder' src="/placeholder.jpg" alt="placeholder.jpg" />
+        <img className='placeholder' src="/placeholder.jpg" alt="placeholder.jpg" />
+      </section>
     </section>
   )
 }
