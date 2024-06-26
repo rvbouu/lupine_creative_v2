7b8c352a75eb3da870ec18ddf8d628939e4e4326
Vanessa Bou <rvbouu@gmail.com>
2024-06-26 03:08:59 -0500

    added button to footer form

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,9 +7,12 @@ export default function Footer() {
     <footer>
       <form id='newsletter'>
         <label className='news-label' htmlFor="news-email" >Subscribe to Lupine Creative Newsletters!</label>
-        <input placeholder='Email' id='news-email' className='input' name="news-email" type="email" required />
+        <div>
+          <input placeholder='Email' id='news-email' className='input' name="news-email" type="email" required />
+          <button id='news-btn' type='submit'>&#x2192;</button>
+        </div>
       </form>
-      <a href='https://www.instagram.com'><img src='/logo_branding/insta_logo_light.png' alt='instagram logo' id='insta-logo'/></a>
+      <a href='https://www.instagram.com'><img src='/logo_branding/insta_logo_light.png' alt='instagram logo' id='insta-logo' /></a>
     </footer>
   )
 }
